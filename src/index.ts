1ae6cff0c9132054517260444b3f7a8d8a14c03a
daiki510 <ddgg7755@gmail.com>
2024-03-25 19:26:13 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ const server = Bun.serve({
         and(
           eq(tasks.id, 1),
           eq(tasks.isCompleted, false),
-          gte(tasks.createdAt, new Date("2024-03-24"))
+          gte(tasks.createdAt, new Date("2024-03-25"))
         )
       );
     console.log(result);
